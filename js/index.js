628fe3c344a20f1017cd61579645f516eeefef3e
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-07-25 18:29:23 -0700

    meta tweaks

---
+++ js/index.js
@@ -0,0 +1,49 @@
+;(() => {
+  const $ = document.body
+  const driftUserId = 1177958
+
+  const chatButton = $.querySelector('#open-chat').addEventListener(
+    'click',
+    () => {
+      drift.on('ready', api => api.sidebar.toggle())
+    }
+  )
+
+  const scheduleButton = $.querySelector(
+    '#schedule-meeting-button'
+  ).addEventListener('click', () => {
+    drift.on('ready', api => api.scheduleMeeting(driftUserId))
+  })
+
+  const newConversationButton = $.querySelector(
+    '#new-conversation-button'
+  ).addEventListener('click', () => {
+    drift.on('ready', api => api.goToNewConversation())
+  })
+
+  const conversationListButton = $.querySelector(
+    '#conversation-list-button'
+  ).addEventListener('click', () => {
+    drift.on('ready', api => {
+      api.goToConversationList()
+    })
+  })
+
+  // A LITTLE EXPERIMENTING
+  // const jokeButton = $.querySelector('#drift-open-chat')
+  //   .addEventListener('click', () => {
+  //     location = '#drift-open-chat'
+  //     drift.on('ready', (api, payload) => {
+  //       api.goToNewConversation()
+  //       window.drift.on("startConversation", function (data) {
+  //         console.log("User started a new conversation " + data.conversationId);
+  //       });
+  //     })
+  //   })
+  const jokeButton = $.querySelector('#drift-open-chat').addEventListener(
+    'click',
+    () => {
+      location = '#drift-open-chat'
+    }
+  )
+})()
